f9650426c558d12fa054593d5c884008b5c21289
Lucas31394 <lucaspfernandes1994@hotmail.com>
2025-10-05 17:05:53 -0300

    working with arrays and type alias

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,4 +1,14 @@
+// Primitive types
 let valor = 3000;
 let nome = "";
 let isPago = false;
 let qualquer = "";
+// Arrays
+const list = [];
+list.push(13, 22.5, 123, 89, 1.58);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
+export {};
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,4 +1,24 @@
+import { TipoTransacao } from "./types/TipoTransacao";
+
+// Primitive types
 let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
+
+// Arrays
+const list: number[] = [];
+list.push(13, 22.5, 123, 89, 1.58);
+
+// Type Alias
+type Transacao = {
+    tipoTransacao: string,
+    data: Date,
+    valor: number
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+}
